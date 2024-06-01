cc1c0bd1a9004acefd66e3ec9db57c339c45fd75
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-01 00:38:23 -0700

    promise ecommerce

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 const div = document.querySelector('#card-div')
 
 axios('https://fakestoreapi.com/products')
-.then((res)=>{
-    console.log(res.data)
-    res.data.map((item)=>{
+.then((res) => {
+    console.log(res.data);
+    res.data.map((item) => {
         div.innerHTML += `
-        <div class="card m-2" style="width: 16rem;">
-        <img width= "1000px" src="${item.image}" class="card-img-top" alt="${item.name}">
-        <div class="card-body">
-        <h5 class="card-title">category : ${item.category}</h5>
-        <h5 class="card-title">Title : ${item.title}</h5>
-        <p class="card-text">Price : ${item.price}</p>
-        <a href="#" class="btn btn-primary">ADD TO CARD</a>
-  </div>
-</div>
-        
-        `
-    })
-    
+        <div class="card h-100% m-2" style="width: 16rem;">
+            <img src="${item.image}" class="card-img-top" alt="${item.title}">
+            <div class="card-body">
+                <h5 class="card-title">Category: ${item.category}</h5>
+                <h5 class="card-title">Title: ${item.title}</h5>
+                <p class="card-text">Price: $${item.price}</p>
+                <a href="#" class="btn btn-primary">ADD TO CART</a>
+            </div>
+        </div>
+        `;
+    });
 })
-.catch((err)=>{
-    console.log(err)
-})
+.catch((err) => {
+    console.log(err);
+});
