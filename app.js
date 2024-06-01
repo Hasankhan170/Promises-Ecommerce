973842918a19737d5d4368ed69f555c6746bfc07
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-01 00:15:54 -0700

    promise ecommerce

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const div = document.querySelector('#card-div')
 axios('https://fakestoreapi.com/products')
 .then((res)=>{
     console.log(res.data)
+    res.data.map((item)=>{
+        div.innerHTML += `
+        <div class="card m-2" style="width: 16rem;">
+        <img width= "1000px" src="${item.image}" class="card-img-top" alt="${item.name}">
+        <div class="card-body">
+        <h5 class="card-title">category : ${item.category}</h5>
+        <h5 class="card-title">Title : ${item.title}</h5>
+        <p class="card-text">Price : ${item.price}</p>
+        <a href="#" class="btn btn-primary">ADD TO CARD</a>
+  </div>
+</div>
+        
+        `
+    })
     
 })
 .catch((err)=>{
